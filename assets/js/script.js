72f431a32e1583c93dc7abe95312da792f1f0db1
Kayla S <kaylaesmith1@gmail.com>
2023-11-03 13:09:31 +0000

    Clean up commented code. Add images for readme. Validate code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,80 @@
-let allCharactersData = [];
-
 // Function to fetch and display characters
 async function fetchCharacters() {
-    // const charactersPerPage = 20;
-    let page = 1; // Start with page 1
-    let allCharacters = []; // Array to store all characters
+  let page = 1;
+  let allCharacters = [];
 
 
-    try {
-        while (true) {
-            const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
-            const data = await response.json();
+  try {
+    while (true) {
+      const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
+      const data = await response.json();
 
-            // Add the characters from the current page to the array
-            allCharacters = allCharacters.concat(data.results);
+      allCharacters = allCharacters.concat(data.results);
 
-            // If there are no more pages, exit the loop
-            if (data.info.next === null) {
-                break;
-            }
+      if (data.info.next === null) {
+        break;
+      }
 
-            // Move to the next page
-            page++;
-        }
-
-        // Get the container where character cards will be displayed
-        const characterContainer = document.getElementById('character-container');
-
-        // Loop through each character in the data
-        allCharacters.forEach(character => {
-
-            // Create a Bootstrap column div for the character card
-            const cardCol = document.createElement('div');
-            cardCol.classList.add('col-12', 'col-md-6', 'col-lg-4');
-            
-            // Create a card element for the character
-            const card = document.createElement('div');
-            card.classList.add('character-card', 'card');
-
-            // Create an image element for the character's avatar
-            const avatar = document.createElement('img');
-            avatar.classList.add('card-img-top');
-            avatar.src = character.image;
-            avatar.alt = character.name;
-
-            // Create elements for name, status, species, and location
-            const cardBody = document.createElement('div');
-            cardBody.classList.add('card-body');
-
-            const name = document.createElement('h2');
-            name.classList.add('card-title');
-            name.textContent = character.name;
-    
-            const status = document.createElement('p');
-            status.classList.add('card-text');
-            status.textContent = `Status: ${character.status}`;
-    
-            const species = document.createElement('p');
-            species.classList.add('card-text');
-            species.textContent = `Species: ${character.species}`;
-    
-            const location = document.createElement('p');
-            location.classList.add('card-text');
-            location.textContent = `Location: ${character.location.name}`;
-
-        // Append elements to the card and cardCol
-        cardBody.appendChild(name);
-        cardBody.appendChild(status);
-        cardBody.appendChild(species);
-        cardBody.appendChild(location);
-
-        card.appendChild(avatar);
-        card.appendChild(cardBody);
-
-        cardCol.appendChild(card);
-
-        // Append the cardCol to the characterContainer
-        characterContainer.appendChild(cardCol);
-        });
-    } catch (error) {
-        console.error('Error fetching data:', error);
+      page++;
     }
+
+    const characterContainer = document.getElementById('character-container');
+
+    // Loop through characters in the API
+    allCharacters.forEach(character => {
+
+      // Create a Bootstrap column div for the character card
+      const cardCol = document.createElement('div');
+      cardCol.classList.add('col-12', 'col-md-6', 'col-lg-4');
+
+      // Create a card element for the character
+      const card = document.createElement('div');
+      card.classList.add('character-card', 'card');
+
+      // Create an image element for the character's avatar
+      const avatar = document.createElement('img');
+      avatar.classList.add('card-img-top');
+      avatar.src = character.image;
+      avatar.alt = character.name;
+
+      // Create elements for name, status, species, and location
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+
+      const name = document.createElement('h2');
+      name.classList.add('card-title');
+      name.textContent = character.name;
+
+      const statusText = document.createElement('p');
+      statusText.classList.add('card-text');
+      statusText.innerHTML = `<u class="card-text-headings">Status</u>: ${character.status}`;
+
+      const speciesText = document.createElement('p');
+      speciesText.classList.add('card-text');
+      speciesText.innerHTML = `<u class="card-text-headings">Species</u>: ${character.species}`;
+
+      const locationText = document.createElement('p');
+      locationText.classList.add('card-text');
+      locationText.innerHTML = `<u class="card-text-headings">Location</u>: ${character.location.name}`;
+
+      cardBody.appendChild(name);
+      cardBody.appendChild(statusText);
+      cardBody.appendChild(speciesText);
+      cardBody.appendChild(locationText);
+
+      card.appendChild(avatar);
+      card.appendChild(cardBody);
+
+      cardCol.appendChild(card);
+
+      characterContainer.appendChild(cardCol);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
 
-// Load DOM fully before fetchCharacters function called
+// LOAD DOM BEFORE CHARACTERS FETCHED
 document.addEventListener('DOMContentLoaded', fetchCharacters);
 
 
@@ -95,17 +86,17 @@ const scrollButton = document.getElementById('scroll-button');
 
 // Show / hide button at 1000px
 function toggleScrollToTopButton() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        scrollToTopButton.style.display = 'block';
-    } else {
-        scrollToTopButton.style.display = 'none';
-    }
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    scrollToTopButton.style.display = 'block';
+  } else {
+    scrollToTopButton.style.display = 'none';
+  }
 }
 
 // Return to top of page
 function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Other browsers
 }
 
 // EventListener: toggles button visibility when scrolling
@@ -118,47 +109,43 @@ scrollButton.addEventListener('click', scrollToTop);
 
 
 // SEARCH A CHARACTER
-// Function to filter and display characters based on search input
 function filterCharacters() {
-    const searchInput = document.getElementById('search-input').value.toLowerCase();
-    const characterCards = document.querySelectorAll('.character-card');
-  
-    characterCards.forEach(card => {
-      const characterName = card.querySelector('.card-title').textContent.toLowerCase();
-      if (characterName.includes(searchInput)) {
-        card.style.display = 'block';
-        
-      } else {
-        card.style.display = 'none';
-      }
-    });
-  }
+  const searchInput = document.getElementById('search-input').value.toLowerCase();
+  const characterCards = document.querySelectorAll('.character-card');
 
-  function resetCharacters() {
-    const characterCards = document.querySelectorAll('.character-card');
-
-    characterCards.forEach(card => {
+  characterCards.forEach(card => {
+    const characterName = card.querySelector('.card-title').textContent.toLowerCase();
+    if (characterName.includes(searchInput)) {
       card.style.display = 'block';
-    });
-  
-    // Clear the search input
-    document.getElementById('search-input').value = '';
-  }
-  
 
-  // EventListener: Listen for input changes in the search bar
-  const searchInput = document.getElementById('search-input');
-  const searchButton = document.getElementById('search-button');
-
-  const resetButton = document.getElementById('reset-button');
-
-  searchButton.addEventListener('click', filterCharacters);
-  resetButton.addEventListener('click', resetCharacters);
-
-  // Optionally, you can also trigger the search when the Enter key is pressed in the input field.
-  searchInput.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-      filterCharacters();
+    } else {
+      card.style.display = 'none';
     }
   });
-  
+}
+
+function resetCharacters() {
+  const characterCards = document.querySelectorAll('.character-card');
+
+  characterCards.forEach(card => {
+    card.style.display = 'block';
+  });
+
+  document.getElementById('search-input').value = '';
+}
+
+//EventListener: search / reset
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+const resetButton = document.getElementById('reset-button');
+
+searchButton.addEventListener('click', filterCharacters);
+resetButton.addEventListener('click', resetCharacters);
+
+//Click 'Enter' to search
+searchInput.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    filterCharacters();
+  }
+});
